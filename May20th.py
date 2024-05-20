4078d0b1c22bb0f5711d6d8398af9533ccd9b5d3
mutuakj <106160177+Mutuakj@users.noreply.github.com>
2024-05-20 13:40:32 -0700

    Python
    This is an introductory program to Python Programming.

---
+++ May20th.py
@@ -0,0 +1,14 @@
+# This programs says Hello
+print('Hello World!')
+# Ask for their name
+print('What is your Name')
+myName = input()
+# Thank them for meeting them
+print('It is good to meet you, ' + myName)
+# Show the length of their name
+print('The length of your name is:')
+print(len(myName))
+# Ask for their age
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
---
+++ Python pr2.py
@@ -0,0 +1,16 @@
+# This program says hello
+print('Hello world!')
+# Ask for their name
+print('What is your name?')
+myName = input()
+print('Glad to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+# Ask for their age
+print('What is your age?')
+myAge = input()
+print('Great! You will be ' + str(int(myAge) + 10) + ' in ten years time.')
+# Ask if they have anything else to say
+print('How may I help you ' + myName)
+
+
